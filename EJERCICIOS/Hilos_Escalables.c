2495f0adc931d5c05f92bc01b941a89dc59eef28
Emilio <159085225+Emiliomuci@users.noreply.github.com>
2025-06-01 22:53:06 +0200

    Create Hilos_Escalables.c

---
+++ EJERCICIOS/Hilos_Escalables.c
@@ -0,0 +1,74 @@
+//El algortimo debe de ser escalavable, debe de funcionar para cualqueir numero de hilos
+	//Crear 7 hilos vivos, y cuando se meta un caracter con getchar, y con se al intro, cada uno de los hilos creados, mueren en el orden ivnerso a su creación.
+	
+	//He nacido. Soy el hilo 1 con ID 2312413243
+	
+	//Getchar
+	
+	//EL hilo numero 7 con ID  e234242 es acabado
+	//Osea el ultimo hilo que se ha creado es el primero en morir
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <signal.h> 
+#include <semaphore.h>
+#include <pthread.h>
+#define NUMEROHILOS 7
+
+void* hilosEscalables (void* arg){
+	int id = *(int*) arg;
+printf("He nacido, soy el hilo %d con ID %d\n", id + 1, pthread_self());
+
+return NULL;	
+}
+
+
+int main(int argc, char **argv)
+{
+	pthread_t hilo[NUMEROHILOS];
+	int ids[NUMEROHILOS];
+	
+	for (int i = 0; i < NUMEROHILOS; i++)
+	{
+		ids[i] = i;
+			pthread_create(&hilo[i],NULL, hilosEscalables, &ids[i]);
+			pthread_join(hilo[i], NULL);
+	}
+	
+	printf("Presiona lo que sea en el teclado: ");
+	getchar();
+	
+	for (int i = NUMEROHILOS - 1; i >= 0; i --)
+	{
+		pthread_cancel(hilo[i]);
+		pthread_join(hilo[i],NULL);
+		
+		printf("El hilo numero %d con ID %d \n", i + 1, hilo[i]);
+
+	}
+	
+	char texto[100];
+	
+	printf("Escribe un texto");
+	scanf("%s",texto);
+	
+	int inversa = strlen(texto);
+	
+	for (int i = inversa - 1; i >= 0; i--)
+	{
+		printf("%c", texto[i]);
+	}
+
+	
+	
+	
+	
+	return 0;
+}
